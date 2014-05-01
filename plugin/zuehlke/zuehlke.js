e1a90f2266441b25c26573e2250b9111ce142ee9
Ben Millo <bmillo@gmail.com>
2014-05-02 00:24:04 +0200

    another way to implement parallax-background; print slide added

--- plugin/zuehlke/zuehlke.js
+++ plugin/zuehlke/zuehlke.js
@@ -1,8 +1,6 @@
 Reveal.addEventListener('slidechanged', function (event) {
     // event.previousSlide, event.currentSlide, event.indexh, event.indexv
 
-
-
     var title = event.currentSlide.attributes.getNamedItem('data-title') || event.currentSlide.parentNode.attributes.getNamedItem('data-title');
     if (typeof title != 'undefined' && title != null) {
         zuehlkeRemoveClass('zuehlke-header', 'hide');
@@ -11,7 +9,14 @@ Reveal.addEventListener('slidechanged', function (event) {
     } else {
         zuehlkeAddClass('zuehlke-header', 'hide');
     }
-});
+    var parallaxStep = event.currentSlide.attributes.getNamedItem('data-parallax-step');
+    if (parallaxStep) {
+        var backgroundXDelta = parallaxStep.value * 200;
+        document.body.style.backgroundPosition = "-" + backgroundXDelta + "px 0";
+    } else {
+        document.body.style.backgroundPosition = "";
+    }
+})
 zuehlkeRemoveClass = function (id, className) {
     var myClassName = " " + className; //must keep a space before class name
     var d;
